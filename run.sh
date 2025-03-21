c5493111b92db5ada6e495346d1fd7753b10fe4e
Decorate3D <decorate3d.2023@gmail.com>
2025-03-21 11:45:38 +0800

    update

---
+++ run.sh
@@ -0,0 +1,4 @@
+CUDA_VISIBLE_DEVICES=0 python test.py --test_dir DIV2K_test_LR_bicubic/X4 --save_dir NTIRE2025_ImageSR_x4/results --model_id 7
+
+# CUDA_VISIBLE_DEVICES=0 python test.py --model_id 7
+
